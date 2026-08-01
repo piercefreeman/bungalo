bc21e34484283c4599666a1b5edd69041bb67860
Pierce Freeman <piercefreeman@gmail.com>
2026-08-01 07:50:23 -0700

    Sync remote backups with source deletions

--- bungalo/backups/remote.py
+++ bungalo/backups/remote.py
@@ -222,7 +222,7 @@ class RCloneSync:
     async def _run_sync(self, src: str, dst: str, update_status: SlackMessage) -> None:
         process = await asyncio.create_subprocess_exec(
             "rclone",
-            "copy",
+            "sync",
             "--config",
             str(self.config_path),
             src,
